fb209fb76cb00a2bc29b1b83913561a0f37e3d3f
Fabien Masson <masson.fabien@gmail.com>
2023-09-10 16:44:17 +0200

    heat map (better example)

--- heatmap_test.go
+++ heatmap_test.go
@@ -14,16 +14,21 @@ func TestHeatMap(t *testing.T) {
 	months := []string{"Jan", "Feb", "Mar", "Apr", "Mai", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"}
 	days := make([]string, 0)
 	for i := 0; i < 31; i++ {
-		days = append(days, fmt.Sprintf("%d", i))
+		days = append(days, fmt.Sprintf("%d", i+1))
 	}
 	for i := 0; i < 12; i++ {
 		activity[i] = make([]float64, 31)
 		for j := 0; j < 31; j++ {
-			if j < 31 || (i != 1 && i != 3 && i != 5 && i != 8 && i != 10) {
-				activity[i][j] = rand.Float64() * 10
-			}
+			activity[i][j] = rand.Float64() * 10
 		}
 	}
+	activity[1][30] = 0.0
+	activity[1][29] = 0.0
+	activity[1][28] = 0.0
+	activity[3][30] = 0.0
+	activity[5][30] = 0.0
+	activity[8][30] = 0.0
+	activity[10][30] = 0.0
 
 	lc := charts.NewHeatMap(
 		800,
